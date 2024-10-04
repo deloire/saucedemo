d8d9e81338e4ca608256a24b83db234b6e9fead6
Александр <lazzero@MacBook-Air.local>
2024-10-04 16:43:37 +0300

    Configurate BaseTest class

---
+++ src/test/java/com/saucedemo/BaseTest.java
@@ -0,0 +1,26 @@
+package com.saucedemo;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseTest {
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1280x720";
+        Configuration.headless = false;
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @BeforeEach
+    public void init() {
+        setUp();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
+
+}
